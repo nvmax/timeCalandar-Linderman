97809a0e22eeadd1a7458d61031d2bfea159fe95
Jerrod Linderman <nvmaxx@gmail.com>
2022-07-19 13:22:08 -0500

    added ptag to index to use as current time down to seconds, cleaned up notes, updated readme.md

--- Readme.md
+++ Readme.md
@@ -1,1 +1,31 @@
+# Time Calandar - Jerrod Linderman
+<div id="header" align="center">
+  <img src="https://media.giphy.com/media/M9gbBd9nbDrOTu1Mqx/giphy.gif" width="100"/>
+</div>
+<div align="center">
+ Jerrod Linderman
+</div>
+<div align="center">
+<img src="https://komarev.com/ghpvc/?username=nvmaxx&style=flat-square&color=blue" alt=""/>
+</div>
+
+
+Daily Time calandar for Jerrod Linderman
+
+Allows user to enter in notes in specific times of the day.  Time slots are color coded based on time being either past present or future.
+
+Data is saved in local storage. User can delete data from local storage using double click on the time slots text area.
+
+Time is refreshed every 60 seconds to check if time has changed
+
+
+
+
+![](/assets/timecalandar.png)
+
+
+
+repo: https://github.com/nvmax/timeCalandar-Linderman
+
+deployed site: https://nvmax.github.io/timeCalandar-Linderman/
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,57 +2,40 @@
 
 $(init);
 
-// need current time and date for index page to display
+// Function to initialize page getting current time and timer for each second
 function init() {
     var now = moment();
     $('#currentDay').text(now.format('MMM  Do  YYYY'));
-
     timeSlots();
     $(".saveBtn").on("click", save);
     //save removed it was saving undefined
     storage();
-    var countdown = setInterval(timeSlots, 60000)
+    var countdown = setInterval(timeSlots, 1000)
     console.log(countdown)
     }
-
-
-    // reference info for moment
-    // Year, month, and day tokens
-    // Tokens are case-sensitive.
-    
-    // Input	Example	Description
-    // YYYY	        2014	4 or 2 digit year. Note: Only 4 digit can be parsed on strict mode
-    // YY	        14	2 digit year
-    // Y	        -25	Year with any number of digits and sign
-    // Q	        1..4	Quarter of year. Sets month to first month in quarter.
-    // M MM	        1..12	Month number
-    // MMM MMMM	    Jan..December	Month name in locale set by moment.locale()
-    // D DD	        1..31	Day of month
-    // Do	        1st..31st	Day of month with ordinal
-    // DDD DDDD	    1..365	Day of year
-    // X	        1410715640.579	Unix timestamp
-    // x	        1410715640579	Unix ms timestamp
-
 
     // need to use past present and future time with in a loop 
     // refacted if statments with info gathered from https://www.youtube.com/watch?v=EumXak7TyQ0
     function timeSlots() {
         $('.time-block').each(function() {
+            // get current time for hour min and second 
+            var currentTime = moment();
+            $('#currentTime').text(currentTime.format('h:mm:ss A'));
             // combined both sections into one parseint to return time
             var time = parseInt($(this).attr('id').split('-')[1]);
             console.log(time);            
             var currenttime = moment().format("H"); 
             console.log(currenttime); // returns hour in military time
             // remove the class of past present and future worked with jessica lane 
-            $(this).removeClass("past present future"); // put back in 
-            if (time < currenttime) return $(this).addClass('past'); // working changing color of past to grey
-            if (time > currenttime) return $(this).addClass('future'); // was not working had === jessica lane fixed it with ==
-            return $(this).addClass('present'); // working changing color of future to green
+            $(this).removeClass("past present future"); // put back in removes classes prior to adding new class
+            if (time < currenttime) return $(this).addClass('past'); // changes color of past
+            if (time > currenttime) return $(this).addClass('future'); // changes color of future 
+            return $(this).addClass('present'); // changes color of present      
         }
         );
     }   
     
-
+// worked with jessica lane and others in zoom call for save and storage functions
 // save function to local storage
 function save() {
     // gets info from ID of index.html
@@ -62,13 +45,13 @@ function save() {
 };
 
 function storage() {
-    // gets info from timblock and loops 
+    // gets info from timblock and loops through to populate textarea from local storage.
     $(".time-block").each(function() {
         var bId = $(this).attr("id");
         $("#" + bId + " textarea").text(localStorage.getItem(moment().format("MDYY") + bId));
     });
 }
-
+// bonus feature remove items from local storage by double clicking
 $(".time-block").on("dblclick", function() {
     var bId = $(this).attr("id");
     $("#" + bId + " textarea").val("");
